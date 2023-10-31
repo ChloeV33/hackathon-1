cfd5ff067b6bf453b8973592805e6574d8e8b992
Denis Bonnand <denis.bonnand@etu.minesparis.psl.eu>
2023-10-31 17:11:53 +0100

    ,

--- empty_notebook.py
+++ empty_notebook.py
@@ -25,36 +25,12 @@
 # sauvé en Python
 
 # %%
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
 
 
 
 # %%
-df = pd.read_csv("media/planetes.csv", skiprows = 96)
-
-df2 = df[["pl_rade", "pl_bmasse"]]
-df2= df2.dropna()
-df2["radius(km)"] = df2["pl_rade"]*6371
-df2["mass(kg)"] = df2["pl_bmasse"]*5.972*10**24
-df2["density"] = df2["mass(kg)"]/((4/3)*3.14159*(df2["radius(km)"]*10**3)**3)
-df2.plot.scatter("radius(km)", "density", loglog = True, marker = ".", s = 1, color = "green" )
-df2.plot.scatter("mass(kg)", "density", loglog = True, marker = ".", s = 1, color = "red" )
-df2.plot.scatter("mass(kg)", "radius(km)", loglog = True, marker = ".", s = 1, color = "blue" )
 
 # %%
-df = pd.read_csv("media/planetes.csv", skiprows = 96)
-df2 = df[["sy_dist", "disc_year"]].dropna()
-df3 = df.groupby(by = "disc_year")
-annees = df['disc_year'].unique()
-dist = []
-for a in annees:
-    dist.append(df3.get_group(a)['sy_dist'].sum())
-plt.plot(annees, dist, marker = 'o', linestyle = '')
-plt.show()
-
 
 
 
